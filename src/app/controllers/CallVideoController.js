daa21998f0f41b72376f1347d0dcbfcc1702bd77
unknown <danchoiphonui27@gmail.com>
2022-04-20 20:57:16 +0700

    add feature callVideo

--- src/app/controllers/CallVideoController.js
+++ src/app/controllers/CallVideoController.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 class CallVideoController {
   index(req, res, next) {
-    res.render("user/callVideo");
+    res.render("user/callVideoView");
   }
 }
 module.exports = new CallVideoController();
