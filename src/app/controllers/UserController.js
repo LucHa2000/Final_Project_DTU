c35724da276a102f3634f72e7c1787c9e4d02292
Nhan Dang <nhan.icdi@gmail.com>
2022-04-25 18:02:00 +0700

    feat: fe homepage, profile, inbox

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,23 +1,24 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-import { getUserById, updateUser } from "../service/UserService";
+import { getUserById, updateUser } from '../service/UserService';
 class UserController {
   async index(req, res, next) {
     const userId = req.session.userID;
     let account = await getUserById(userId);
-    res.render("user/edit_profile", { account: account });
+    // res.render('user/edit_profile', { account: account });
+    res.render('user/profile', { account: account });
   }
   async changeAccount(req, res, next) {
     if (!req.file) {
-      req.body.image = "";
+      req.body.image = '';
     } else {
-      req.body.image = req.file.path.split("\\").slice(3).join();
+      req.body.image = req.file.path.split('\\').slice(3).join();
     }
     let accountChanged = await updateUser(req.body);
     if (accountChanged) {
-      res.redirect("back");
+      res.redirect('back');
     } else {
-      console.log("Change failed !");
+      console.log('Change failed !');
     }
   }
 }
