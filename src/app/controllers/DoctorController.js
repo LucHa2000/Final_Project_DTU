23b7dbfeb673104a3fb11e599f800a938f74f322
Nhan Dang <nhan.icdi@gmail.com>
2022-04-27 19:40:52 +0700

    feat: fe doctor profile, work history, resume

--- src/app/controllers/DoctorController.js
+++ src/app/controllers/DoctorController.js
@@ -1,46 +1,47 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-import { getAppointmentsByUserID } from "../service/AppoinmentService";
-import { getUserById, updateUser } from "../service/UserService";
-import { getResumeById, updateResume } from "../service/DoctorService";
+import { getAppointmentsByUserID } from '../service/AppoinmentService';
+import { getUserById, updateUser } from '../service/UserService';
+import { getResumeById, updateResume } from '../service/DoctorService';
 
 class DoctorController {
   async index(req, res, next) {
     const userId = req.session.userID;
     let account = await getUserById(userId);
-    res.render("doctor/edit_profile", { account: account });
+    // res.render("doctor/edit_profile", { account: account });
+    res.render('doctor/profile', { account: account });
   }
   async changeAccountDoctor(req, res, next) {
     if (!req.file) {
-      req.body.image = "";
+      req.body.image = '';
     } else {
-      req.body.image = req.file.path.split("\\").slice(3).join();
+      req.body.image = req.file.path.split('\\').slice(3).join();
     }
     let accountChanged = await updateUser(req.body);
     if (accountChanged) {
-      res.redirect("back");
+      res.redirect('back');
     } else {
-      console.log("Change failed !");
+      console.log('Change failed !');
     }
   }
   //render workHistory Page
   async workHistory(req, res) {
     let appointments = await getAppointmentsByUserID(req.session.userID, 2);
     console.log(appointments);
-    res.render("doctor/workHistory", { appointments: appointments });
+    res.render('doctor/workHistory', { appointments: appointments });
   }
   async doctorResume(req, res) {
     let resume = await getResumeById(req.session.resumeID);
-    res.render("doctor/resumeDoctor", { resume: resume });
+    res.render('doctor/resumeDoctor', { resume: resume });
   }
   async changeResume(req, res) {
     console.log(req.body.description.trim());
     let resumeUpdated = await updateResume(req.body);
 
     if (resumeUpdated) {
-      res.redirect("back");
+      res.redirect('back');
     } else {
-      console.log("update failed !");
+      console.log('update failed !');
       return;
     }
   }
--- src/public/script/inbox.js
+++ src/public/script/inbox.js
@@ -1,34 +1,34 @@
-let room = "";
-let sender = "";
-let roomID = "";
+let room = '';
+let sender = '';
+let roomID = '';
 // display chatting Area
 
 $(document).ready(() => {
   //get room and sender
-  room = $("#roomName").text().trim();
-  roomID = $("#roomID").text().trim();
+  room = $('#roomName').text().trim();
+  roomID = $('#roomID').text().trim();
   //send room to Server
-  socket.emit("create-room", room);
+  socket.emit('create-room', room);
 
-  let senderID = $(".accountID").text().trim();
-  let senderName = $(".accountName").text().trim();
+  let senderID = $('.accountID').text().trim();
+  let senderName = $('.accountName').text().trim();
   //show chat container
 
-  $("#send-message").prop("disabled", true);
+  $('#send-message').prop('disabled', true);
   //check message change
-  $("#message-inbox").keyup(function () {
+  $('#message-inbox').keyup(function () {
     let content = $(this).val();
-    if (content != "") {
-      $("#send-message").prop("disabled", false);
+    if (content != '') {
+      $('#send-message').prop('disabled', false);
     } else {
-      $("#send-message").prop("disabled", true);
+      $('#send-message').prop('disabled', true);
     }
   });
 
   //send message
 
-  $("#send-message").click(function (e) {
-    let message = $("#message-inbox").val();
+  $('#send-message').click(function (e) {
+    let message = $('#message-inbox').val();
     //let room = $("#room-name").text();
     let content = {
       message: message,
@@ -38,46 +38,46 @@ $(document).ready(() => {
       roomID: roomID.trim(),
     };
     //emit to server
-    socket.emit("user-chat", content);
+    socket.emit('user-chat', content);
 
     //submit message to the server
 
     $.ajax({
-      type: "POST",
-      url: "http://localhost:4000/inbox/saveMessage",
+      type: 'POST',
+      url: 'http://localhost:4000/inbox/saveMessage',
       data: content,
-      dataType: "json",
+      dataType: 'json',
       encode: true,
     }).done(function (data) {
       console.log(data);
     });
-    $("#message-inbox").val("");
-    $("#send-message").prop("disabled", false);
+    $('#message-inbox').val('');
+    $('#send-message').prop('disabled', false);
   });
 });
 
 //listening list rooms from Server
-socket.on("server-send-rooms", (data) => {
+socket.on('server-send-rooms', (data) => {
   // console.log(data);
 });
 
 //listening room now
-socket.on("server-send-room-socket", (data) => {
-  $("#room-name").html(data);
+socket.on('server-send-room-socket', (data) => {
+  $('#room-name').html(data);
 });
 
 //listening message from server
-socket.on("server-send-chat", (data) => {
-  $(".chat-container").append(
-    '<li class="collection-item"><h1>' +
+socket.on('server-send-chat', (data) => {
+  $('.chat-container').append(
+    '<li class="collection-item my-4"><h1>' +
       '</h1><p class="nameUser font-bold text-sm">' +
       data.senderName +
-      "</p>" +
+      '</p>' +
       data.message +
-      "</li>"
+      '</li>'
   );
-  $("#xconversation-container").animate(
-    { scrollTop: $("#xconversation-container").prop("scrollHeight") },
+  $('#xconversation-container').animate(
+    { scrollTop: $('#xconversation-container').prop('scrollHeight') },
     1000
   );
 });
